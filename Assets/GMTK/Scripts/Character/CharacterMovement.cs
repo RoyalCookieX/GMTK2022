feef8fd4ac5a1b3cc7bea047ad1f1a5ffc28635c
Andrei Cabungcal <40614905+RoyalCookieX@users.noreply.github.com>
2022-07-15 18:51:40 -0500

    Player can now look around and move

--- Assets/GMTK/Scripts/Character/CharacterMovement.cs
+++ Assets/GMTK/Scripts/Character/CharacterMovement.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public abstract class CharacterMovement : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    
+    public Vector3 MoveDirection { get; private set; }
+    public float Yaw { get; private set; }
+
+    [Header("Components")]
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    [Header("Properties")]
+    [SerializeField] private float _moveSpeed = 50;
+    [SerializeField] private float _yawSpeed = 50;
+
+    public void SetMoveDirection(Vector2 localDirection)
+    {
+        MoveDirection = new Vector3(localDirection.x, 0f, localDirection.y) * _moveSpeed;
+    }
+
+    public void AddYaw(float yaw)
+    {
+        Yaw += yaw * _yawSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddRelativeForce(MoveDirection, ForceMode.Acceleration);
+        _rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * Yaw * Time.deltaTime));
+    }
 }
--- Assets/GMTK/Scripts/Character/PlayerController.cs
+++ Assets/GMTK/Scripts/Character/PlayerController.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInputController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    [Header("Compoents")]
+    [SerializeField] private CharacterMovement _characterMovement;
+
     private void OnMovement(InputValue value)
     {
-        
+        _characterMovement.SetMoveDirection(value.Get<Vector2>());
     }
 
     private void OnLook(InputValue value)
     {
-
+        Vector2 lookDelta = value.Get<Vector2>();
+        _characterMovement.AddYaw(lookDelta.x);
     }
 
     private void OnJump(InputValue value)
