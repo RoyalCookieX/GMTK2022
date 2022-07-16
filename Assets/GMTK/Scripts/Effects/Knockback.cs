a77ccac0038f2a035e6871a47200e0b2e40cea46
Koda <ville.koda@gmail.com>
2022-07-16 16:20:24 -0700

    Started Adding Knockback as static methods

---
+++ Assets/GMTK/Scripts/Effects/Knockback.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public static void ExplosionKnockback()
+    {
+
+    }
+
+    public static void TranslateKnockback()
+    {
+
+    }
+}
