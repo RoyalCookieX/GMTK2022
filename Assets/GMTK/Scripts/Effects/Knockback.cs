11a81bf7965d1e33a7561032338790a87f40441e
Koda <ville.koda@gmail.com>
2022-07-16 16:33:20 -0700

    Knockback Static methods

--- Assets/GMTK/Scripts/Effects/Knockback.cs
+++ Assets/GMTK/Scripts/Effects/Knockback.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Knockback : MonoBehaviour
 {
-    public static void ExplosionKnockback()
+    /// <summary>
+    /// Creates a Exxplosion force
+    /// </summary>
+    /// <param name="target">Target affected</param>
+    /// <param name="position">Position it creates</param>
+    /// <param name="force">Strenght of knockback</param>
+    /// <param name="radius">Radious of explosion</param>
+    /// <param name="upwardsForce">Upwards modifier</param>
+    /// <param name="mode">Force mode</param>
+    public static void ExplosionKnockback(in GameObject target, in Vector3 position, in float force, in float radius, in float upwardsForce = 0f, in ForceMode mode = ForceMode.Force)
     {
-
+        if (target.TryGetComponent(out Rigidbody rb))
+        {
+            rb.AddExplosionForce(force, position, radius, upwardsForce, mode);
+        }
     }
 
-    public static void TranslateKnockback()
+    /// <summary>
+    /// Translates object
+    /// </summary>
+    /// <param name="target">Object to translate</param>
+    /// <param name="direction">Direction to translate</param>
+    /// <param name="force">Force of Translation</param>
+    public static void TranslateKnockback(in GameObject target, in Vector3 direction, in float force)
     {
-
+        target.transform.Translate(direction * force);
     }
 }
